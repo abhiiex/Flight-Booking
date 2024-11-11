8a9dfdc6517d4dabb388341ef2f3362278bd2044
satyendra2013 <satyendrapandit2013@gmail.com>
2024-11-11 21:35:01 +0530

    Footer Updated

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -12,9 +12,9 @@ function Footer() {
   let date = new Date();
   let year = date.getFullYear();
   return (
-      <div className=" bg-neutral-500 rounded-[30px] my-5 flex py-5 px-5  flex-col">
-        <Col className="footer-copywright 2xl:text-center">
-          <h3 className="text-[1em]  text-black">Designed and Developed by Avishek Kr Pandit</h3>
+      <div className=" rounded-[30px] my-5 py-5">
+        <Col className="w-full h-fit max-w-[1800px] pl-8 2xl:text-center">
+          <h3 className="text-[1em]  text-black pl-2">Designed and Developed by Avishek Kr Pandit And Team.</h3>
           <span className="text-[1em] flex justify-center text-black  sm:-mt-[25px] ">Copyright © {year} AP</span>
         </Col>  
       </div>
